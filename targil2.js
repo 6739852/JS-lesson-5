a1bdd6cbfb16af311fc66ac53a9d35eb8a02262b
neomi <6739852@gmail.com>
2025-01-22 04:35:00 +0200

    second commit with targil2

---
+++ targil2.js
@@ -0,0 +1,36 @@
+function func1()
+{
+    const names=['Alice','Bob','Tiff','Bruce','Alice']
+    const result=names.reduce((prev,cur)=>{
+        if(prev[cur]==null)
+          prev[cur]=0
+        prev[cur]++
+        return prev
+    },{})
+    return result
+}
+console.log(func1())
+
+function func2()
+{
+    const letters=['a','b','a','b','c','e','e','c','d','d','d','d']
+    const result=letters.reduce((prev,cur)=>{ 
+        if(!prev.includes(cur))
+           prev.push(cur)
+        return prev
+    },[])
+    return result
+}
+console.log(func2())
+
+function func3()
+{
+    const numbers=[-5,25,-19,0,16,0]
+    const result=numbers.reduce((prev,cur)=>{ 
+        if(cur>0)
+          prev.push(Math.sqrt(cur))
+        return prev
+    },[])
+    return result
+}
+console.log(func3())
